01d28e948db66853387e700db833e6b3f0a4ea51
Sascha <sascha@serenitylabs.cloud>
2018-11-09 03:22:52 +0100

    add version flags / repo

---
+++ config/initializers/serenitylabs.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+module Mastodon
+  module Version
+    module_function
+    def repository
+      'serenitylaboratories/mastodon'
+    end
+
+    def flags
+      'sl'
+    end
+  end
+end

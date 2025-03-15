239f36fca0a82ece64879c65fa864f8832484679
ida <ida@is.nota.live>
2025-03-15 16:46:07 +0000

    fixup! parent 7ce8f4af507ec0b31e63a92cb3c7e450b4d2eb2a author ida <ida@is.nota.live> 1715288278 -0700 committer ida <ida@is.nota.live> 1742048444 +0000

--- spec/requests/api/v1/accounts/credentials_spec.rb
+++ spec/requests/api/v1/accounts/credentials_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe 'credentials API' do
     end
 
     describe 'with invalid data' do
-      let(:params) { { note: 'This is too long. ' * 30 } }
+      let(:params) { { note: 'This is too long. ' * 90 } }
 
       it 'returns http unprocessable entity' do
         subject
